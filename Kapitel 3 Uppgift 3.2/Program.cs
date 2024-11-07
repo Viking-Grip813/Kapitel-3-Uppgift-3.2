6705293b1167edd2b39d65233c5780e9cd0947b8
vist07001 <vist07001@E-PW05CGR8>
2024-11-07 12:50:42 +0100

    Add project files.

---
+++ Kapitel 3 Uppgift 3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift3_2
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Har du gått ut gymnasiet?");
+            Console.WriteLine("Svara J för ja och N för nej.");
+            string J = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder < 22)
+            {
+                Console.WriteLine("Vi letar tyvär efter anna personal just nu.");
+            }
+            else
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            
+        }
+    }
+}
